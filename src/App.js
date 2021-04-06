f7ba53017dedec2303dfa55508ccc29d1582a4a0
AlaBenalaya <benalayaaladine@gmail.com>
2021-04-06 15:54:10 +0200

    first change

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Club from './Profil/profil'
+import Nava from './Navbar/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Nava/>
+      <Club/>
+    </div>
+  );
+}
+
+export default App;
